17bf69181693d96197285932e9ebb08bd2e2f8d7
Akshatha Gopinath <akshatha@Akshathas-MacBook-Pro.local>
2021-03-30 11:26:19 +0200

    anged functionality of the 'divide' function

--- code.py
+++ code.py
@@ -1,2 +1,4 @@
-def divide(a, b):
-	return a/b
+def divide(a, b, out='quo'):
+	# out = 'quo' to return quotient, or out='rem' to retuern remainder of the division operation
+	if out=='quo': return a/b
+	if out=='rem': return a%b	
